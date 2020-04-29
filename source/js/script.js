c7a890354b86eb33d77558f473245c7d6e6c8cf5
Kotaina <kotaina217+git@gmail.com>
2020-04-29 17:33:54 +0300

    Исправление ошибок, прочее

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,18 @@
-var open = document.querySelector(".header__link-menu")
-var close = document.querySelector(".header__link-menu--close")
+// var open = document.querySelector(".header__link-menu")
+// var close = document.querySelector(".header__link-menu--close")
+
+// open.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   menu--hide.classList.add("visually-hidden")
+//   menu--hide.classList.add("")
+// }
+// )
+
+var open = document.querySelector(".header__link-menu");
+var close = document.querySelector(".header__link-menu--close");
 
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menu--hide.classList.add("visually-hidden")
-  menu--hide.classList.add("")
-}
-)
+  open.classList.add("header__link-menu--close");
+  close.classList.remove("header__link-menu");
+})
