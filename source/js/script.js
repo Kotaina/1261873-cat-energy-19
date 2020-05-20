0771638ff5cb39891aefa3049c49dff2f6a53e03
Kotaina <kotaina217+git@gmail.com>
2020-05-21 00:01:09 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-var MainNav = document.querySelector(".header__main-nav");
-var MainNavBtn = document.querySelector(".header__link-menu");
+var mainnav = document.querySelector(".header__main-nav");
+var mainnavbtn = document.querySelector(".header__link-menu");
 
-MainNav.classList.remove("header__main-nav--no-js");
+mainnav.classList.remove("header__main-nav--no-js");
 
-MainNavBtn.addEventListener("click", function () {
-  if (MainNav.classList.contains("header__main-nav--closed")) {
-    MainNav.classList.remove("header__main-nav--closed");
-    MainNav.classList.add("header__main-nav--opened");
+mainnavbtn.addEventListener("click", function () {
+  if (mainnav.classList.contains("header__main-nav--closed")) {
+    mainnav.classList.remove("header__main-nav--closed");
+    mainnav.classList.add("header__main-nav--opened");
   } else {
-    MainNav.classList.add("header__main-nav--closed");
-    MainNav.classList.remove("header__main-nav--opened");
+    mainnav.classList.add("header__main-nav--closed");
+    mainnav.classList.remove("header__main-nav--opened");
   }
 });
